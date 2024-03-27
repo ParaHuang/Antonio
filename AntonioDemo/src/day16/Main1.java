be4f53f4639e1194cd00fc9dfe20a50808377fec
ParaHuang <paradox041@gmail.com>
2024-03-27 15:19:27 +0800

    inital submission

---
+++ AntonioDemo/src/day16/Main1.java
@@ -0,0 +1,30 @@
+package day16;
+
+public class Main1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// ASCII code
+		char c1 = 'a';
+		System.out.println(c1);
+		System.out.println((int)c1);
+		
+		char c2 = 'a';
+		System.out.println(c2);
+		System.out.println((int)c2);
+		
+		System.out.println(c2>=48 && c2<=57);
+		System.out.println((char)48);
+		System.out.println((char)57);
+		System.out.println(c2>='0' && c2<='9');//prove a char save a value as a number
+	
+	
+		char letter = '~';
+		System.out.println(letter>='a' && letter<='z');
+		
+		
+	
+	}
+
+}
+
